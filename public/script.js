c860da940d5adbfe39358a727f46d2d5cf14bf31
Patrick <njorogepatrick739@gmail.com>
2024-07-19 17:59:11 +0300

    submit

--- public/script.js
+++ public/script.js
@@ -1,19 +1,46 @@
-const homeBtn = document.getElementById("home-btn");
+const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
+const homeBtn = document.getElementById("home-btn");
 const aboutBtn = document.getElementById("about-btn");
 const serviceBtn = document.getElementById("service-btn");
 const contactBtn = document.getElementById("contact-btn");
 
-homeBtn.addEventListener("click", (e) => {
+menuBtn.addEventListener("click", (e) => {
   navLinks.classList.toggle("open");
 
   const isOpen = navLinks.classList.contains("open");
-  homeBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-home-line");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
 navLinks.addEventListener("click", (e) => {
   navLinks.classList.remove("open");
-  homeBtnIcon.setAttribute("class", "ri-home-line");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content1 figcaption", {
+  ...scrollRevealOption,
+  delay: 1000,
 });
 
+ScrollReveal().reveal(".accessories image", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal("accesories2 image", {
+  duration: 1000,
+});
 
